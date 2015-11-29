c195a5d248666c60fd88d4934ff4e6e3be60c0e9
Symon L <Symon L>
2015-11-29 00:54:20 -0500

    Final Version

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,30 +1,25 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>1-800 Hotline Bling</title>
-        <meta name = "viewport" content ="width=device-width, initial-scale=1.0">
-        <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
-        <link href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel = "stylesheet">
-        <link href = "./css/styles.css" rel = "stylesheet">
-        <script href="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-    </head>
+<head>
+    <title>1-800 Hotline Bling</title>
+    <meta name = "viewport" content ="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+    <link href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel = "stylesheet">
+    <link href = "./css/styles.css" rel = "stylesheet">
+    <script href="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+</head>
 
-    <body>
-<<<<<<< HEAD
-        <div class = "container" >
-=======
-    <!-- Main page, 3 -->
-        <div class = "container" id = "firstbackground">
->>>>>>> 34eae76a48315c4c7c8fa6f861303a657e2f7edf
-            <div class = "text-center">
-                <h1>Plan A Date</h1>
-                <p><a href="{{ url('/family') }}">Family</a></p>
-                <p><a href="{{ url('/friends') }}">Friend</a></p>
-                <p><a href="{{ url('/partner') }}">Partner</a></p>
-            </div>
-        </div>
-    </body>
+<body>
+<div class = "container" >
+    <div class = "text-center">
+        <h1>Plan A Date</h1>
+        <p><a href="{{ url('/family') }}">Family</a></p>
+        <p><a href="{{ url('/friends') }}">Friend</a></p>
+        <p><a href="{{ url('/partner') }}">Partner</a></p>
+    </div>
+</div>
+</body>
 </html>
